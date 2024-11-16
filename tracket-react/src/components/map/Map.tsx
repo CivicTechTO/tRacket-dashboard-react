c60aab8e181eca16641e1aa58932047515051281
Kaung Zin Hein <kaungzinhein113@gmail.com>
2024-11-16 13:24:32 -0500

    feat(map): Add clusters and tooltips

--- tracket-react/src/components/map/Map.tsx
+++ tracket-react/src/components/map/Map.tsx
@@ -1,4 +1,9 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Tooltip,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import {
   LAYER_ATTRIBUTION,
@@ -7,11 +12,53 @@ import {
   MAP_CENTER_LON,
   ZOOM,
 } from "../../config";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import { useEffect, useState } from "react";
+import { getLocations } from "../api/locations";
+import { type Location } from "../../types/api";
+import { type LatLngTuple } from "leaflet";
 
 const Map = () => {
+  const [locations, setLocations] = useState<Location[]>([]);
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      try {
+        const result = await getLocations();
+        setLocations(result.locations);
+      } catch (err: unknown) {
+        console.error(err);
+      }
+    };
+
+    fetchLocations();
+  }, []);
+
   return (
-    <MapContainer center={[MAP_CENTER_LAT, MAP_CENTER_LON]} zoom={ZOOM}>
+    <MapContainer center={[MAP_CENTER_LAT, MAP_CENTER_LON]} zoom={ZOOM}  zoomControl={true}>
       <TileLayer url={LAYER_URL} attribution={LAYER_ATTRIBUTION} />
+
+      <MarkerClusterGroup>
+        {locations.map((l) => {
+          const position: LatLngTuple = [l.latitude, l.longitude];
+          const toolTipActive = l.active
+            ? "Active Location"
+            : "Inactive Location";
+          const toolTipLabel = l.label || "";
+
+          return (
+            <Marker position={position} key={l.id}>
+              <Tooltip>
+                <div>
+                  <b>{toolTipActive}</b>
+                  <br></br>
+                  {toolTipLabel}
+                </div>
+              </Tooltip>
+            </Marker>
+          );
+        })}
+      </MarkerClusterGroup>
     </MapContainer>
   );
 };
