0f72f567f919908697049725307e2a4553c3e254
MaanasArora <maanasarora23@gmail.com>
2025-01-26 19:44:05 +0530

    Add raw data chart to location page

--- tracket-react/src/components/pages/Location.tsx
+++ tracket-react/src/components/pages/Location.tsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getLocationNoiseData, getLocations } from "../api/locations";
-import { Location, TimedLocationNoiseData, Granularity } from "../../types/api";
+import {
+  Location,
+  TimedLocationNoiseData,
+  Granularity,
+  NoiseTimed,
+} from "../../types/api";
 import { CheckCircleIcon, NoSymbolIcon } from "@heroicons/react/24/solid";
 import dayjs from "dayjs";
-import { CircleMarker, MapContainer, TileLayer } from "react-leaflet";
+import { Circle, MapContainer, TileLayer, Tooltip } from "react-leaflet";
 import { LAYER_ATTRIBUTION, LAYER_URL } from "../../config";
+import {
+  Area,
+  CartesianGrid,
+  ComposedChart,
+  Line,
+  ResponsiveContainer,
+  XAxis,
+} from "recharts";
 
 const dateFormat = "YYYY-MM-DD HH:mm:ss";
 
@@ -52,21 +65,21 @@ const NoiseMeterChart = ({
 
 const MapView = ({ location }: { location: Location }) => {
   return (
-    <div className="w-1/2 h-full p-20 shadow-log rounded-xl">
+    <div className="w-1/3 h-full p-8 shadow-log rounded-xl">
       <MapContainer
         style={{ height: "100%", width: "100%", borderRadius: "1rem" }}
         center={[location.latitude, location.longitude]}
-        zoom={30}
+        zoom={20}
         scrollWheelZoom={false}
         dragging={false}
       >
         <TileLayer url={LAYER_URL} attribution={LAYER_ATTRIBUTION} />
-        <CircleMarker
+        <Circle
           center={[location.latitude, location.longitude]}
           radius={location.radius}
         >
           <span>{location.label}</span>
-        </CircleMarker>
+        </Circle>
       </MapContainer>
     </div>
   );
@@ -93,16 +106,45 @@ const LocationDetails = ({ location }: { location: Location }) => (
   </div>
 );
 
+const NoiseChart = ({ measurements }: { measurements: NoiseTimed[] }) => (
+  <ResponsiveContainer height={100}>
+    <ComposedChart
+      data={
+        measurements.map((measurement) => ({
+          mean: measurement.mean,
+          minMax: [measurement.min, measurement.max],
+          timestamp: dayjs(measurement.timestamp).valueOf(),
+        })) || []
+      }
+      margin={{ top: 5, right: 5, left: 5, bottom: 5 }}
+    >
+      <Line type="monotone" dataKey="mean" stroke="#8884d8" dot={false} />
+      <Area type="monotone" dataKey="minMax" stroke="#8884d8" fill="#8884d8" />
+      <CartesianGrid stroke="#ccc" />
+      <XAxis
+        dataKey="timestamp"
+        ticks={[...Array(3).keys()].map((i) =>
+          dayjs(
+            measurements[Math.floor(measurements.length / 3 - 1) * i].timestamp
+          ).valueOf()
+        )}
+        tickFormatter={(time) => dayjs(time).format("YYYY-MM-DD HH:mm")}
+      />
+      <Tooltip />
+    </ComposedChart>
+  </ResponsiveContainer>
+);
+
 const NoiseData = ({
   noiseData,
+  rawNoiseData,
 }: {
   noiseData: TimedLocationNoiseData | undefined;
+  rawNoiseData: TimedLocationNoiseData | undefined;
 }) => {
-  if (!noiseData) {
+  if (!noiseData || !rawNoiseData) {
     return (
-      <div className="w-1/2 flex justify-center items-center">
-        Loading...
-      </div>
+      <div className="w-2/3 flex justify-center items-center">Loading...</div>
     );
   }
 
@@ -112,77 +154,98 @@ const NoiseData = ({
 
   const latestNoise = noiseData.measurements.slice(-1)[0];
 
-  const last7days = noiseData.measurements.slice(-7 * 24);
+  const last7days = noiseData.measurements.slice(-14 * 24);
   const mean7Days =
     last7days.reduce((acc, curr) => acc + curr.mean, 0) / last7days.length;
 
   const last7daysDay = last7days.filter(
-    (measurement) => dayjs(measurement.timestamp).hour() >= 6
+    (measurement) =>
+      dayjs(measurement.timestamp).hour() >= 7 &&
+      dayjs(measurement.timestamp).hour() < 19
   );
   const mean7DaysDay =
     last7daysDay.reduce((acc, curr) => acc + curr.mean, 0) /
     last7daysDay.length;
 
   const last7daysEvening = last7days.filter(
-    (measurement) => dayjs(measurement.timestamp).hour() >= 18
+    (measurement) =>
+      dayjs(measurement.timestamp).hour() >= 19 &&
+      dayjs(measurement.timestamp).hour() < 23
   );
   const mean7DaysEvening =
     last7daysEvening.reduce((acc, curr) => acc + curr.mean, 0) /
     last7daysEvening.length;
 
   const last7daysNight = last7days.filter(
-    (measurement) => dayjs(measurement.timestamp).hour() >= 0
+    (measurement) =>
+      dayjs(measurement.timestamp).hour() >= 23 ||
+      dayjs(measurement.timestamp).hour() < 7
   );
   const mean7DaysNight =
     last7daysNight.reduce((acc, curr) => acc + curr.mean, 0) /
     last7daysNight.length;
 
-  console.log(noiseData.measurements.slice(-7 * 24).length);
-
   return (
-    <div className="w-1/2">
-      <div className="w-full flex justify-between bg-white rounded-xl p-4 shadow-lg mb-5">
-        <div className="w-1/2 p-4">
-          <h2>
-            <span className="font-medium text-xl mr-2">Last Hour</span> Average
-            Noise
-          </h2>
-          <NoiseMeterChart noiseValue={latestNoise.mean} />
-          <p className="text-3xl mb-2">{latestNoise.mean.toFixed(1)} dB</p>
+    <div className="w-2/3 p-4">
+      <div className="w-full flex gap-4">
+        <div className="w-1/3 flex justify-between bg-white rounded-xl p-4 shadow-lg mb-5">
+          <div className="w-full p-4">
+            <h2>
+              <span className="font-medium text-xl mr-2">Last Hour</span>{" "}
+              Average Noise
+            </h2>
+            <NoiseMeterChart noiseValue={latestNoise.mean} />
+            <p className="text-3xl mb-2">{latestNoise.mean.toFixed(1)} dB</p>
+            <p className="text-sm text-gray-500 mt-5">Last updated</p>
+            <p className="text-md">
+              {dayjs(latestNoise.timestamp).format(dateFormat)}
+            </p>
+          </div>
         </div>
-        <div className="w-1/2 p-4">
-          <p className="text-sm text-gray-500 mb-1">Last updated</p>
-          <p className="text-md mb-2">
-            {dayjs(latestNoise.timestamp).format(dateFormat)}
-          </p>
+        <div className="w-2/3 flex justify-between bg-white rounded-xl p-4 shadow-lg mb-5">
+          <div className="w-1/2 p-4">
+            <h2 className="mb-2">
+              <span className="font-medium text-xl mr-1">Last 14 Days</span>{" "}
+              Average Noise
+            </h2>
+            <NoiseMeterChart noiseValue={mean7Days} />
+            <p className="text-3xl mb-2">{mean7Days.toFixed(1)} dB</p>
+          </div>
+          <div className="w-1/2 p-4">
+            <h3>
+              <span className="font-medium">Day</span>
+              (07:00 - 19:00)
+            </h3>
+            <NoiseMeterChart noiseValue={mean7DaysDay} showTicks={false} />
+            <p className="text-xl mb-2">{mean7DaysDay.toFixed(1)} dB</p>
+
+            <h3>
+              <span className="font-medium">Evening</span>
+              (19:00 - 23:00)
+            </h3>
+            <NoiseMeterChart noiseValue={mean7DaysEvening} showTicks={false} />
+            <p className="text-xl mb-2">{mean7DaysEvening.toFixed(1)} dB</p>
+
+            <h3>
+              <span className="font-medium">Night</span>
+              (23:00 - 07:00)
+            </h3>
+            <NoiseMeterChart noiseValue={mean7DaysNight} showTicks={false} />
+            <p className="text-xl mb-2">{mean7DaysNight.toFixed(1)} dB</p>
+          </div>
         </div>
       </div>
-      <div className="w-full flex justify-between bg-white rounded-xl p-4 shadow-lg mb-5">
-        <div className="w-1/2 p-4">
-          <h2 className="mb-2">
-            <span className="font-medium text-xl mr-1">Last 7 Days</span>{" "}
-            Average Noise
-          </h2>
-          <NoiseMeterChart noiseValue={mean7Days} />
-          <p className="text-3xl mb-2">{mean7Days.toFixed(1)} dB</p>
-        </div>
-        <div className="w-1/2 p-4">
-          <h3 className="font-medium">Day</h3>
-          <NoiseMeterChart noiseValue={mean7DaysDay} showTicks={false} />
-          <p className="text-xl mb-2">{mean7DaysDay.toFixed(1)} dB</p>
-
-          <h3 className="font-medium">Evening</h3>
-          <NoiseMeterChart noiseValue={mean7DaysEvening} showTicks={false} />
-          <p className="text-xl mb-2">{mean7DaysEvening.toFixed(1)} dB</p>
-
-          <h3 className="font-medium">Night</h3>
-          <NoiseMeterChart noiseValue={mean7DaysNight} showTicks={false} />
-          <p className="text-xl mb-2">{mean7DaysNight.toFixed(1)} dB</p>
+      <div className="w-full flex">
+        <div className="w-full bg-white rounded-xl p-4 shadow-lg">
+          <div className="w-full p-4">
+            <h2 className="text-xl mb-4">Noise Data</h2>
+            <NoiseChart measurements={rawNoiseData.measurements} />
+            <button className="mt-1 bg-emerald-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus">
+              Download as CSV
+            </button>
+          </div>
         </div>
       </div>
-      <button className="bg-blue-500 text-white p-2 rounded-md shadow-md">
-        Download all data as CSV
-      </button>
     </div>
   );
 };
@@ -192,6 +255,7 @@ const LocationPage = () => {
 
   const [location, setLocation] = useState<Location>();
   const [noiseData, setNoiseData] = useState<TimedLocationNoiseData>();
+  const [rawNoiseData, setRawNoiseData] = useState<TimedLocationNoiseData>();
   const [notFound, setNotFound] = useState<boolean>(false);
 
   const fetchLocations = async () => {
@@ -217,7 +281,16 @@ const LocationPage = () => {
       granularity: Granularity.Hourly,
     })) as TimedLocationNoiseData;
 
+    const latestNoise = noiseData.measurements.slice(-1)[0];
+    const lastTimestamp = latestNoise.timestamp;
+
+    const rawNoiseData = (await getLocationNoiseData(parseInt(id), {
+      granularity: Granularity.Raw,
+      start: dayjs(lastTimestamp).subtract(3, "days").toISOString(),
+    })) as TimedLocationNoiseData;
+
     setNoiseData(noiseData);
+    setRawNoiseData(rawNoiseData);
   };
 
   useEffect(() => {
@@ -231,7 +304,7 @@ const LocationPage = () => {
       </div>
       <div className="grow flex md:flex-row flex-col justify-around items-center bg-gradient-to-b from-emerald-500 to-gray-100 px-16 py-8">
         <MapView location={location} />
-        <NoiseData noiseData={noiseData} />
+        <NoiseData noiseData={noiseData} rawNoiseData={rawNoiseData} />
       </div>
     </div>
   ) : (
