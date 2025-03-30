54fca1364cd0eaba3f1507ccdc75f6015f5add27
MaanasArora <maanasarora23@gmail.com>
2025-03-29 23:06:33 -0400

    Move files to top level

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest/config" />
 
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     // dataLoading tests can take > 5s
     testTimeout: 10_000,
