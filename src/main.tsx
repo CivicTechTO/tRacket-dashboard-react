c972d25f1f5d9b2b84543c302063bccc45a3a285
MaanasArora <maanasarora23@gmail.com>
2025-05-11 18:51:22 -0400

    Move to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { Routes, Route, HashRouter } from 'react-router';
 
 import Map from './components/map/Map';
 import LocationPage from './components/pages/Location';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/tRacket-dashboard-react'>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Map />} />
         <Route path='/location/:id' element={<LocationPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
