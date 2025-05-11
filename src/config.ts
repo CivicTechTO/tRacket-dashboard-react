4eed034f0ff5e7d7203fce60c4c6c43184312e73
MaanasArora <maanasarora23@gmail.com>
2025-05-11 18:55:55 -0400

    Move to OpenStreetMap for maps

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
-export const LAYER_URL =
-  'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png';
+export const LAYER_URL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 export const LAYER_ATTRIBUTION =
-  '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>';
+  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
 export const MAP_CENTER_LAT = 43.6701;
 export const MAP_CENTER_LON = -79.4293;
